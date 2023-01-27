cf49e333a8f4c0dcfffc543d992144451897bba8
fernandamoncayo <123701497+fernandamoncayo@users.noreply.github.com>
2023-01-27 18:51:00 -0300

    Delete cv.js

--- cv.js
+++
@@ -1,3 +0,0 @@
-window.onload = function() {
-    alert('Hola. Soy Sofía. Estoy buscando nuevas oportunidades laborales. Al final de mi CV puedes dejar tus datos y propuesta laboral, y a la brevedad me contactaré. Muchas gracias!')
-}
