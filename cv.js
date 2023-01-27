869df7cf1bf48e8a411a09b609b112bc73f65da7
fernandamoncayo <123701497+fernandamoncayo@users.noreply.github.com>
2023-01-27 13:35:34 -0300

    Add files via upload

---
+++ cv.js
@@ -0,0 +1,3 @@
+window.onload = function() {
+    alert('Hola. Soy Sofía. Estoy buscando nuevas oportunidades laborales. Al final de mi CV puedes dejar tus datos y propuesta laboral, y a la brevedad me contactaré. Muchas gracias!')
+}
